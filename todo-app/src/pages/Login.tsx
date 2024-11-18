5604b41783f1757d37d137be0c046ca8be2fc271
LexadoF <65349887+LexadoF@users.noreply.github.com>
2024-11-18 10:28:16 -0500

    eliminado el menu del login

--- todo-app/src/pages/Login.tsx
+++ todo-app/src/pages/Login.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSplitPane, IonButtons, IonMenuButton } from '@ionic/react';
-import Menu from '../components/Menu';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSplitPane } from '@ionic/react';
 
 const Login: React.FC = () => (
   <IonSplitPane contentId="main">
     <IonPage id="main">
       <IonHeader>
         <IonToolbar>
-          <IonButtons slot='start'>
-          <IonMenuButton />
-          </IonButtons >
           <IonTitle>Login</IonTitle>
         </IonToolbar>
       </IonHeader>
