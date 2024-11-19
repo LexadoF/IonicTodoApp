93b169495bd59b2bc8186a948cbbe93d38e26292
LexadoF <65349887+LexadoF@users.noreply.github.com>
2024-11-19 13:01:11 -0500

    navegacion nueva tarea

--- todo-app/src/pages/NewTask.tsx
+++ todo-app/src/pages/NewTask.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonButton, IonInput, IonTextarea, IonSelect, IonSelectOption } from '@ionic/react';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonButton, IonInput, IonTextarea, IonSelect, IonSelectOption, IonIcon } from '@ionic/react';
 import { useHistory } from 'react-router';
+import { arrowBack } from 'ionicons/icons';
 
 const NewTask: React.FC = () => {
   const history = useHistory();
 
   const handleSave = () => {
     history.push('/tasks');
-};
+  };
+
+  const navigateToTasks = () => {
+    history.push('/tasks');
+  };
 
   return (
     <IonPage>
@@ -22,6 +27,9 @@ const NewTask: React.FC = () => {
       <IonContent>
         <div className="page-wrapper">
           <div className="page-form">
+            <IonButton fill="clear" className="back-arrow" onClick={navigateToTasks}>
+              <IonIcon icon={arrowBack} slot="icon-only" />
+            </IonButton>
             <h2>Nueva tarea</h2>
             <label htmlFor="title">Titulo</label>
             <IonInput type="text" id="title" placeholder="Titulo" className="input-field" />
