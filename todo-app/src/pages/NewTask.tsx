1d3c98040ff1c89d60f830a96328af973dafbc16
LexadoF <65349887+LexadoF@users.noreply.github.com>
2024-11-19 10:37:48 -0500

    vista nueva tarea

--- todo-app/src/pages/NewTask.tsx
+++ todo-app/src/pages/NewTask.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton } from '@ionic/react';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonMenuButton, IonButton, IonCheckbox, IonInput, IonText, IonTextarea } from '@ionic/react';
 
 const NewTask: React.FC = () => (
   <IonPage>
@@ -8,11 +8,30 @@ const NewTask: React.FC = () => (
         <IonButtons slot='start'>
           <IonMenuButton />
         </IonButtons >
-        <IonTitle>NewTask</IonTitle>
+        <IonTitle>Nueva tarea</IonTitle>
       </IonToolbar>
     </IonHeader>
     <IonContent>
-      <h1>NewTask Page</h1>
+      <div className="page-wrapper">
+        <div className="page-form">
+          <h2>Nueva tarea</h2>
+          <label htmlFor="title">Titulo</label>
+          <IonInput type="text" id="title" placeholder="Titulo" className="input-field" />
+
+          <label htmlFor="description">Descripción</label>
+          <IonTextarea id="description" placeholder="Ingrese su correo" className="input-field" />
+
+          <label htmlFor="date">Fecha</label>
+          <IonInput type="date" id="date" placeholder="Ingrese su correo" />
+
+          <label htmlFor="priority">Prioridad</label>
+          <div className="password-field">
+            <IonInput type="password" id="priority" placeholder="Prioridad" className="input-field" />
+          </div>
+
+          <IonButton expand="block" className="continue-register-button btn_primary_custom">Guardar</IonButton>
+        </div>
+      </div>
     </IonContent>
   </IonPage>
 );
